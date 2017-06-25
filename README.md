597126d002a7444b080018e57f452d0d4ad7f97f
mikeninpl <mikeninpl@gmail>
2017-06-25 20:04:00 +0200

    putting_all_together

--- README.md
+++
@@ -1,9 +0,0 @@
-![alt text](https://github.com/msdxbelux/XamarinAlliance/blob/master/covr.png "Logo Title Text 1")
-# XamarinAlliance
-
-#### A multi challenge experience for developers to build their first connected Xamarin application.
-www.xamarinalliance.com
-
-Questions or issues? Check out the [FAQ](https://github.com/msdxbelux/XamarinAlliance/blob/master/FAQ.md) or [log an issue](https://github.com/msdxbelux/XamarinAlliance/issues)
-
-Organized by the MSFT Belux DX team
---
+++ src/XamarinAlliance/XamarinAllianceApp/Views/Main.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAllianceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public void OnButtonClicked(object sender, EventArgs args)
+        {
+            ((Button)sender).Text =
+                String.Format("{0}", "Done");
+        }
+    }
+}
